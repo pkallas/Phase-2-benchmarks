54157dc13ab0686e9dd8c928e8d48bebf9580def
pkallas <pkallas@Patricks-MacBook-Pro.local>
2017-07-26 11:25:35 -0700

    Added alternative method to movie search cli, and notes and explanation to bottom of original method

--- Movie_Search_CLI/movie-search.js
+++ Movie_Search_CLI/movie-search.js
@@ -1,6 +1,6 @@
 
 // path = 'find/?ref_=nv_sr_fn&q=userquery&s=all'
-// ?ref_=nv_sr_fn&q=process.argv[2] &s=all --> us qs.stringify to turn these into an input, so user can search using special characters
+// ?ref_=nv_sr_fn&q=process.argv[2]&s=all --> use qs.stringify to turn these into an input, so user can search using special characters
 /* {ref_ : 'nv_sr_fn',
     q: input,
     s: 'all'}
@@ -13,11 +13,10 @@ const imdbPath = qs.stringify(
     {ref_ : 'nv_sr_fn',
     q: input,
     s: 'tt'});
-// Use port 80 for http
+// Use port 80 for http, that's the default; not necessary to declare the port unless it is different than 80
 const req = http.request({
   hostname: 'www.imdb.com',
-  path:'/find?' + imdbPath,
-  port: 80
+  path:'/find?' + imdbPath
 }, (res) => {
   let str = ''
   res.on('data', function(chunk) {
@@ -27,10 +26,18 @@ const req = http.request({
     const $ = cheerio.load(str);
     //<div class ='findSection'>, <table class='findList'>, <td class='result_text'>
     // class we want is 'result_text'
-    let titles = $('.result_text').text();
+    let titles = $('.result_text').map((i, elm) => $(elm).text()).toArray().join('\n');
     //Return result in console display --> result
     console.log(titles);
   })
 })
 
 req.end()
+
+/* let titles = $('.result_text').text(); --> alternative way of using cheerio to
+get the text you want; this method does not allow the text to be mapped to an Array,
+however, so while simpler, it's not as useful. The map method above requires passing
+in the i argument even though it is never used; refer to cheerio docs for more info;
+toArray() is called to display the relevant info in an array, and join('\n') is
+called to display the movie titles each on a new line, in a more human readable
+format */
